901220165e1857a1a6ca4931bd6bcfe5cb153d26
Sean A Clarke <sean.alexander.clarke@gmail.com>
2018-07-13 16:26:04 -0700

    Continued Development

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-function Table() {
-  this.rows = [{}];
-  this.cols = 0;
-
-  this.add_col = function (hdr, dflt='-') {
-    this.rows[0][this.cols] = hdr;
-    for (var row in this.rows.slice(1)) {
-        row++;
-        this.rows[row][hdr] = dflt;
+function Table(name) {
+    this.name = name;
+    this.rows = [{}];
+    this.cols = 0;
+    this.add_col = function (hdr, dflt='-') {
+        this.rows[0][this.cols] = hdr;
+        for (var row in this.rows.slice(1)) {
+            row++;
+            this.rows[row][hdr] = dflt;
+        }
+        this.cols += 1;
     }
-    this.cols += 1;
-  }
-  this.add_row = function (v) {
-    nr = {};	
-    for (var hdr in this.rows[0]) {
-      nr[this.rows[0][hdr]] = v[hdr];
+    this.add_row = function (v) {
+        nr = {};	
+        for (var hdr in this.rows[0]) {
+        nr[this.rows[0][hdr]] = v[hdr];
+        }
+        this.rows.push(nr)
     }
-    this.rows.push(nr)
-  }
+    this.rmv_col = function () {
+        // pass
+    }
+    this.rmv_row = function () {
+        // pass
+    }
 }
 
 function displayColors(o) {
@@ -24,6 +30,55 @@ function displayColors(o) {
     var color_selection = document.createElement('div');
     color_selection.setAttribute('id', name.concat('-controls-color-tab'));
     color_selection.setAttribute('class', 'table-controls-color-tab');
+    color_selection.style.display = 'grid';
+    var color_selection_sky = document.createElement('button');
+    color_selection_sky.setAttribute('id', name.concat('-controls-color-button-sky'));
+    color_selection_sky.setAttribute('class', 'table-controls-color-button');
+    color_selection_sky.setAttribute('onclick', 'changeColors("sky");');
+    color_selection_sky.style.background = 'linear-gradient(to right, #3399DD, #1177DD)';
+    var color_selection_sea = document.createElement('button');
+    color_selection_sea.setAttribute('id', name.concat('-controls-color-button-sea'));
+    color_selection_sea.setAttribute('class', 'table-controls-color-button');
+    color_selection_sea.setAttribute('onclick', 'changeColors("sea");');
+    color_selection_sea.style.background = 'linear-gradient(to right, #00BBA0, #00ABC0)';
+    var color_selection_jungle = document.createElement('button');
+    color_selection_jungle.setAttribute('id', name.concat('-controls-color-button-jungle'));
+    color_selection_jungle.setAttribute('class', 'table-controls-color-button');
+    color_selection_jungle.setAttribute('onclick', 'changeColors("jungle");');
+    color_selection_jungle.style.background = 'linear-gradient(to right, #77BB77, #448844)';
+    var color_selection_ocean = document.createElement('button');
+    color_selection_ocean.setAttribute('id', name.concat('-controls-color-button-ocean'));
+    color_selection_ocean.setAttribute('class', 'table-controls-color-button');
+    color_selection_ocean.setAttribute('onclick', 'changeColors("ocean");');
+    color_selection_ocean.style.background = 'linear-gradient(to right, #1060A0, #005070)';
+    var color_selection_sunset = document.createElement('button');
+    color_selection_sunset.setAttribute('id', name.concat('-controls-color-button-sunset'));
+    color_selection_sunset.setAttribute('class', 'table-controls-color-button');
+    color_selection_sunset.setAttribute('onclick', 'changeColors("sunset");');
+    color_selection_sunset.style.background = 'linear-gradient(to right, #FF5540, #EE3340)';
+    var color_selection_berry = document.createElement('button');
+    color_selection_berry.setAttribute('id', name.concat('-controls-color-button-berry'));
+    color_selection_berry.setAttribute('class', 'table-controls-color-button');
+    color_selection_berry.setAttribute('onclick', 'changeColors("berry");');
+    color_selection_berry.style.background = 'linear-gradient(to right, #CC2288, #441188)';
+    var color_selection_night = document.createElement('button');
+    color_selection_night.setAttribute('id', name.concat('-controls-color-button-night'));
+    color_selection_night.setAttribute('class', 'table-controls-color-button');
+    color_selection_night.setAttribute('onclick', 'changeColors("night");');
+    color_selection_night.style.background = 'linear-gradient(to right, #445566, #223344)';
+    var color_selection_marble = document.createElement('button');
+    color_selection_marble.setAttribute('id', name.concat('-controls-color-button-marble'));
+    color_selection_marble.setAttribute('class', 'table-controls-color-button');
+    color_selection_marble.setAttribute('onclick', 'changeColors("marble");');
+    color_selection_marble.style.background = 'linear-gradient(to right, white, #DDDDDD)';
+    color_selection.appendChild(color_selection_sky);
+    color_selection.appendChild(color_selection_sea);
+    color_selection.appendChild(color_selection_jungle);
+    color_selection.appendChild(color_selection_ocean);
+    color_selection.appendChild(color_selection_sunset);
+    color_selection.appendChild(color_selection_berry);
+    color_selection.appendChild(color_selection_night);
+    color_selection.appendChild(color_selection_marble);
     o.appendChild(color_selection);
 }
 
@@ -35,7 +90,44 @@ function hideColors() {
     }
 }
 
-function changeColors(c_pal) {
+function changeColors(str) {
+    // set col palletes:    lt       dk      aux-pri    aux-sec    sec-lt     sec-dk
+    var cpal_marble = ['#E9E9E9', '#D5D5D5', '#EFEFEF', 'white', '#D9D9D9', '#C5C5C5'];
+    var cpal_sunset = ['#FF5540', '#EE3340', '#FF893F', '#000000', '#FF7060', '#EE5050'];
+    var cpal_berry = ['#CC2288', '#441188', '#BB22BB', '#000000', '#CC4499', '#4433AA'];
+    var cpal_sky = ['#3399DD', '#1177DD', '#0060C0', '#000000', '#44AAFF', '#2288EE'];
+    var cpal_ocean = ['#1060A0', '#005070', '#0090CC', '#00A0E0', '#1080C0', '#007090'];
+    var cpal_sea = ['#00BBA0', '#00ABC0', '#009BA0', '#000000', '#22D0BB', '#00BBEE'];
+    var cpal_jungle = ['#77BB77', '#448844', '#007700', '#000000', '#88CC88', '#509950'];
+    var cpal_night = ['#445566', '#223344', '#7090B0', '#80A0C0', '#607585', '#324354'];
+    document.documentElement.style.setProperty('--table-text-color', 'white');
+    switch(str) {
+        case 'night':
+            c_pal = cpal_night;
+            break;
+        case 'ocean':
+            c_pal = cpal_ocean;
+            break;
+        case 'sky':
+            c_pal = cpal_sky;
+            break;
+        case 'berry':
+            c_pal = cpal_berry;
+            break;
+        case 'sea':
+            c_pal = cpal_sea;
+            break;
+        case 'sunset':
+            c_pal = cpal_sunset;
+            break;
+        case 'jungle':
+            c_pal = cpal_jungle;
+            break;
+        case 'marble':
+            c_pal = cpal_marble;
+            document.documentElement.style.setProperty('--table-text-color', '#555555');
+            break;
+    }
     document.documentElement.style.setProperty('--table-lt-color', c_pal[0]);
     document.documentElement.style.setProperty('--table-dk-color', c_pal[1]);
     document.documentElement.style.setProperty('--table-aux-pri-color', c_pal[2]);
@@ -45,17 +137,17 @@ function changeColors(c_pal) {
 }
 
 function displayLayouts(o) {
-  var layout_selection = document.createElement('div');
-  layout_selection.setAttribute('class', 'table-controls-layout-tab');
-  o.appendChild(layout_selection);
+    var layout_selection = document.createElement('div');
+    layout_selection.setAttribute('class', 'table-controls-layout-tab');
+    o.appendChild(layout_selection);
 }
 
 function hideLayouts() {
-  var layout_selections = document.getElementsByClassName('table-controls-layout-tab');
-  for (var l_select in layout_selections) {
-    layout_selections[l_select].parentNode.removeChild(layout_selections[l_select]);
-    return
-  }
+    var layout_selections = document.getElementsByClassName('table-controls-layout-tab');
+    for (var l_select in layout_selections) {
+        layout_selections[l_select].parentNode.removeChild(layout_selections[l_select]);
+        return;
+    }
 }
 
 function alternatingRowsLayout(name) {
@@ -65,7 +157,8 @@ function alternatingRowsLayout(name) {
     }
 }
 
-function addTable(t, name) {
+function addTable(t) {
+    name = t.name;
     var tables = document.getElementById('tables-container');
     // Create Table Top
     var t_top = document.createElement('div');
@@ -82,7 +175,7 @@ function addTable(t, name) {
     t_top_search_input.setAttribute('type', 'text');
     t_top_search_input.setAttribute('autocomplete', 'off');
     t_top_search_input.setAttribute('placeholder', 'Search...');
-    t_top_search.appendChild(t_top_search_input)
+    t_top_search.appendChild(t_top_search_input);
     var t_top_color = document.createElement('div');
     t_top_color.setAttribute('id', name.concat('-controls-color'));
     t_top_color.setAttribute('class', 'table-controls-color');
@@ -99,6 +192,7 @@ function addTable(t, name) {
     var t_top_delete = document.createElement('button');
     t_top_delete.setAttribute('id', name.concat('-controls-delete'));
     t_top_delete.setAttribute('class', 'table-controls-delete');
+    t_top_delete.setAttribute('onclick', 'removeTable(this.id.slice(0, -16));');
     t_top.appendChild(t_top_delete);
     t_top.appendChild(t_top_edit);
     t_top.appendChild(t_top_layout);
@@ -115,11 +209,16 @@ function addTable(t, name) {
     var tr_html = document.createElement('tr');
     for (var hdr in t.rows[0]) {
         var th_html = document.createElement('th');
+        th_html.setAttribute('id', name.concat('-th-').concat(t.rows[0][hdr].replace(" ", "-").toLowerCase()));
+        th_html.setAttribute('class', 'table-th');
         th_html.innerHTML = t.rows[0][hdr];
         tr_html.appendChild(th_html);
     }
     var th_html = document.createElement('th');
-    th_html.innerHTML = 'Actions';
+    var new_col_button_html = document.createElement('button');
+    new_col_button_html.setAttribute('id', name.concat('new-column-button'));
+    new_col_button_html.setAttribute('class', 'table-new-column-button');
+    th_html.appendChild(new_col_button_html);
     tr_html.appendChild(th_html);
     thead_html.appendChild(tr_html);
     t_html.appendChild(thead_html);
@@ -138,19 +237,23 @@ function addTable(t, name) {
     t_html.appendChild(tfoot_html);
     // Create Table Body
     var tbody_html = document.createElement('tbody');
-    for (var row in t.rows.slice(1)) {
-        row++;
+    for (i = 1; i < t.rows.length; i++) {
         var tr_html = document.createElement('tr');
-        if (row % 2 == 1) {
+        if (i % 2 == 1) {
             tr_html.className += 'table-row-odd'
         }
-        for (var cell in t.rows[row]) {
+        for (var cell in t.rows[i]) {
             var td_html = document.createElement('td');
-            td_html.innerHTML = t.rows[row][cell];
+            td_html.innerHTML = t.rows[i][cell];
             tr_html.appendChild(td_html);
         }
-        var tr_edit_html = document.createElement('td');
-        tr_html.appendChild(tr_edit_html);
+        var tr_actions_html = document.createElement('td');
+        tr_actions_html.setAttribute('class', 'table-row-actions');
+        var tr_actions_button_html = document.createElement('button');
+        tr_actions_button_html.setAttribute('id', name.concat('-row-').concat(i.toString()).concat('-actions-button'));
+        tr_actions_button_html.setAttribute('class', 'table-row-actions-button');
+        tr_actions_html.appendChild(tr_actions_button_html);
+        tr_html.appendChild(tr_actions_html);
         tbody_html.appendChild(tr_html);
     }
     t_html.appendChild(tbody_html);
@@ -173,34 +276,49 @@ function addTable(t, name) {
 
     tables.appendChild(t_container);
 }
-// set col palletes:    lt       dk      aux-pri    aux-sec    sec-lt     sec-dk  
-var cpal_sunset = ['#FF5540', '#EE3340', '#FF893F', '#000000', '#FF7060', '#EE5050'];
-var cpal_berry = ['#CC2288', '#441188', '#BB22BB', '#000000', '#CC4499', '#4433AA'];
-var cpal_sky = ['#3399DD', '#1177DD', '#0060C0', '#000000', '#44AAFF', '#2288EE'];
-var cpal_ocean = ['#1060A0', '#005070', '#0090CC', '#00A0E0', '#1080C0', '#007090'];
-var cpal_sea = ['#00BBA0', '#00ABC0', '#009BA0', '#000000', '#22D0BB', '#00BBEE'];
-var cpal_jungle = ['#77BB77', '#448844', '#007700', '#000000', '#88CC88', '#509950'];
-var cpal_night = ['#445566', '#223344', '#7090B0', '#80A0C0', '#607585', '#324354'];
 
-var t = new Table();
-t.add_col('Year');
-t.add_col('Season');
-t.add_col('Host');
-t.add_col('City');
-t.add_col('Continent');
-t.add_row([2022, 'winter', 'China', 'Beijing', 'Asia']);
-t.add_row([2020, 'summer', 'Japan', 'Tokyo', 'Asia']);
-t.add_row([2018, 'winter', 'Korea', 'Pyeongchang', 'Asia']);
-t.add_row([2016,'summer','Brazil','Rio de Janeiro', 'South America']);
-t.add_row([2014, 'winter', 'Russia', 'Sochi', 'Europe']);
-t.add_row([2012,'summer','United Kingdom','London', 'Europe']);
-t.add_row([2010, 'winter', 'Canada', 'Vancouver', 'North America']);
-t.add_row([2008, 'summer', 'China', 'Beijing', 'Asia']);
-t.add_row([2006, 'winter', 'Italy', 'Torino', 'Europe']);
-t.add_row([2004, 'summer', 'Greece', 'Athens', 'Europe']);
-t.add_row([2002, 'winter', 'United States', 'Salt Lake City', 'North America']);
-t.add_row([2000, 'summer', 'Australia', 'Sydney', 'Oceania']);
-t.add_row([1998, 'winter', 'Japan', 'Nagano', 'Asia']);
-t.add_row([1996, 'summer', 'United States', 'Atlanta', 'North America']);
+function removeTable(name) {
+    var tables = document.getElementById('tables-container');
+    var table = document.getElementById(name.concat('-container'));
+    tables.removeChild(table);
+}
 
-addTable(t, 'olympics-table');
+var ot = new Table('olympics-table');
+ot.add_col('Year');
+ot.add_col('Season');
+ot.add_col('Host');
+ot.add_col('City');
+ot.add_col('Continent');
+ot.add_row([2022, 'winter', 'China', 'Beijing', 'Asia']);
+ot.add_row([2020, 'summer', 'Japan', 'Tokyo', 'Asia']);
+ot.add_row([2018, 'winter', 'Korea', 'Pyeongchang', 'Asia']);
+ot.add_row([2016,'summer','Brazil','Rio de Janeiro', 'South America']);
+ot.add_row([2014, 'winter', 'Russia', 'Sochi', 'Europe']);
+ot.add_row([2012,'summer','United Kingdom','London', 'Europe']);
+ot.add_row([2010, 'winter', 'Canada', 'Vancouver', 'North America']);
+ot.add_row([2008, 'summer', 'China', 'Beijing', 'Asia']);
+ot.add_row([2006, 'winter', 'Italy', 'Torino', 'Europe']);
+ot.add_row([2004, 'summer', 'Greece', 'Athens', 'Europe']);
+ot.add_row([2002, 'winter', 'United States', 'Salt Lake City', 'North America']);
+ot.add_row([2000, 'summer', 'Australia', 'Sydney', 'Oceania']);
+ot.add_row([1998, 'winter', 'Japan', 'Nagano', 'Asia']);
+ot.add_row([1996, 'summer', 'United States', 'Atlanta', 'North America']);
+
+var at = new Table('animals-table');
+at.add_col('Name');
+at.add_col('Class');
+at.add_col('Diet');
+at.add_col('Ecosystem');
+at.add_row(['Gorilla', 'Mammal', 'Herbivore', 'Terrestrial']);
+at.add_row(['Blue Whale', 'Mammal', 'Carnivore', 'Aquatic']);
+at.add_row(['Cheetah', 'Mammal', 'Carnivore', 'Terrestrial']);
+at.add_row(['Moose', 'Mammal', 'Herbivore', 'Terrestrial']);
+at.add_row(['Grizzly Bear', 'Mammal', 'Omnivore', 'Terrestrial']);
+at.add_row(['African Dwarf Frog', 'Amphibian', 'Carnivore', 'Aquatic']);
+at.add_row(['Sea Turtle', 'Reptile', 'Omnivorous', 'Aquatic']);
+
+var et = new Table('empty-table');
+
+addTable(ot);
+addTable(at);
+addTable(et);
